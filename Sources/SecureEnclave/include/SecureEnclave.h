5b3cab3c42a85dc76d078b20ee39252ee141d8cb
Keziah Biermann <development@kizzycode.de>
2025-08-11 15:23:26 +0200

    Replace normal type signatures with typedef plus symbols

--- Sources/SecureEnclave/include/SecureEnclave.h
+++ Sources/SecureEnclave/include/SecureEnclave.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-
 /**
  * @brief A stack-allocated fixed-size buffer for data transfer
  */
@@ -18,7 +17,6 @@ typedef struct {
      */
     uint8_t bytes[2040];
 } sep_buf_t;
-
 
 /**
  * @brief The required permissions to use a key
@@ -58,7 +56,6 @@ typedef enum {
     sep_permissions_needs_same_biometry = 5
 } sep_permissions_t;
 
-
 /**
  * @brief Generates a new secure enclave backed P256 private key
  *
@@ -67,7 +64,9 @@ typedef enum {
  * @param error A buffer to write potential error information into
  * @return int `0` on success or `-1` on error
  */
-int sep_p256_generate(sep_permissions_t permissions, sep_buf_t* key, sep_buf_t* error);
+typedef int (sep_p256_generate_t)(sep_permissions_t permissions, sep_buf_t* key, sep_buf_t* error);
+sep_p256_generate_t sep_p256_generate;
+
 /**
  * @brief Gets the associated public key in uncompressed SEC 1 representation (`0x04 || x || y`)
  *
@@ -76,7 +75,9 @@ int sep_p256_generate(sep_permissions_t permissions, sep_buf_t* key, sep_buf_t* 
  * @param error A buffer to write potential error information into
  * @return int `0` on success or `-1` on error
  */
-int sep_p256_publickey(const sep_buf_t* key, sep_buf_t* publickey, sep_buf_t* error);
+typedef int (sep_p256_publickey_t)(const sep_buf_t* key, sep_buf_t* publickey, sep_buf_t* error);
+sep_p256_publickey_t sep_p256_publickey;
+
 /**
  * @brief Derives an ECDH shared secret in uncompressed SEC 1 representation (`0x04 || x || y`)
  *
@@ -86,7 +87,9 @@ int sep_p256_publickey(const sep_buf_t* key, sep_buf_t* publickey, sep_buf_t* er
  * @param error A buffer to write potential error information into
  * @return int `0` on success or `-1` on error
  */
-int sep_p256_keyexchange(const sep_buf_t* key, const sep_buf_t* other, sep_buf_t* dhsecret, sep_buf_t* error);
+typedef int (sep_p256_keyexchange_t)(const sep_buf_t* key, const sep_buf_t* other, sep_buf_t* dhsecret, sep_buf_t* error);
+sep_p256_keyexchange_t sep_p256_keyexchange;
+
 /**
  * @brief Generates an ECDSA signature for the given hash value in it's raw representation (`r || s`,
  *        see https://tools.ietf.org/html/rfc4754)
@@ -97,7 +100,7 @@ int sep_p256_keyexchange(const sep_buf_t* key, const sep_buf_t* other, sep_buf_t
  * @param error A buffer to write potential error information into
  * @return int `0` on success or `-1` on error
  */
-int sep_p256_signhash(const sep_buf_t* key, const sep_buf_t* hash, sep_buf_t* ecdsasig, sep_buf_t* error);
-
+typedef int (sep_p256_signhash_t)(const sep_buf_t* key, const sep_buf_t* hash, sep_buf_t* ecdsasig, sep_buf_t* error);
+sep_p256_signhash_t sep_p256_signhash;
 
 #endif /* SecureEnclave_h */
